d7124445d4c6a95bd317a4e102691c28409f3dd6
prashant makavana <prashantmpb26@gmail.com>
2018-12-08 21:29:53 +0530

    angular tour of heroes demo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Prashant'},
+  {id: 12, name: 'Mr. QA'},
+  {id: 13, name: 'Mr. DEV'},
+  {id: 14, name: 'Mr. BA'},
+  {id: 15, name: 'Mr. PA'},
+];
